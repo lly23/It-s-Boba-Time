e816c0f782e183f2590b682eecd61040c5d07d70
Linh Ly <lly23@live.unc.edu>
2018-11-28 20:37:44 -0500

    added title page

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function() {
+    // $('#strawberry').click(function(e) {
+    //     e.preventDefault();
+    //     $('#intro').css('display','block');
+    //     $('html','body').animate({
+    //         scrollTop: $("#intro").offset().top
+    //       }, 500);
+    // });
+    
+});
