363b51b02278788a682253a4a4e300eb0eb78375
Linh Ly <lly23@live.unc.edu>
2018-12-03 09:22:18 -0500

    Youtube API and Foursquare API

--- js/script.js
+++ js/script.js
@@ -1,59 +1,34 @@
 $(function() {
-    // $('#strawberry').click(function(e) {
-    //     e.preventDefault();
-    //     $('#intro').css('display','block');
-    //     $('html','body').animate({
-    //         scrollTop: $("#intro").offset().top
-    //       }, 500);
-    // });
-
-    // Parallax bubbles
-    var intro = document.getElementById('intro');
-    var parallaxInstance = new Parallax(intro);
+    // FourSquare API
+    fetch('https://api.foursquare.com/v2/venues/explore?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20180323&limit=1&ll=40.7243,-74.0018&query=coffee')
+    .then(function() {
+        // Code for handling API response
+    })
+    .catch(function() {
+        // Code for handling errors
+    });
 
     $(window).scroll(function() {
         var scrollTop = $(window).scrollTop();
-        var height = $(window).height();
+        // var height = $(window).height();
+        console.log(scrollTop);
+        if (scrollTop >= $('#intro').offset().top) {
+            $('#nav').css('display', 'block');
+        }
 
+        if (scrollTop <= $('#intro').offset().top) {
+            $('#nav').css('display', 'none');
+        }
         // $('#title').css({
         //     'opacity': ((height - scrollTop) / height)
         // });
     });
+
+    // Parallax bubbles
+    var red = document.getElementById('red');
+    var parallaxInstance = new Parallax(red);
     
-    var map = kartograph.map('#map');
+    // var map = kartograph.map('#map');
 
-    // Youtube API
-    var tag = document.createElement('script');
-
-    tag.src = "https://www.youtube.com/iframe_api";
-     var firstScriptTag = document.getElementsByTagName('script')[0];
-    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-    var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-          height: '390',
-          width: '640',
-          videoId: 'M7lc1UVf-VE',
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-      }
-
-    function onPlayerReady(event) {
-    event.target.playVideo();
-    }
-
-    var done = false;
-    function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
-    }
-    }
-    function stopVideo() {
-    player.stopVideo();
-    }    
+  
 });
