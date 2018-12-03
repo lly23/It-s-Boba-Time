491b20e348feee0618d63969cead929a71a62b5c
Linh Ly <lly23@live.unc.edu>
2018-12-03 14:33:11 -0500

    created helper functions

--- js/script.js
+++ js/script.js
@@ -2,11 +2,6 @@ $(function() {
     // location is global variable, will change based on user input
     var location = 'Chapel Hill, NC';
 
-    $('#search').click(function() {
-        var userInput = $('#location').val();
-        location = userInput;
-    });
-
     // Geocode
     var locationData = '';
     $.ajax({
@@ -34,64 +29,75 @@ $(function() {
         dataType: 'json',
         async: true,
         success: function(foursquaredata) {
-            // show all venues under query 'tea'
-            console.log(foursquaredata.response.venues);
-            // check first filter item
-            for (var i = 0; i < filters.length; i++) {
-                var filterItem = filters[i];
-                // go through all venues
-                foursquaredata.response.venues.forEach(venue => {
-                    // create variable to store shop name 
-                    var shop = venue.name;
-
-                    // go into categories list
-                    for (var j = 0; j < venue.categories.length; j++) {
-                        // get each name
-                        var name = venue.categories[j].name;
-                        // if name matches filter item then add it to boba results array
-                        if (name == filterItem) {
-                            bobaResults.push(shop);
-                        }
-                    }
-
-                    // go into location
-
-                    // create variable to store full address and reset each time looping through each locatino so that additional locations do not add onto html variable before pushing into array
-                    var fullAddress = '';
-
-                    // get location parts of venue
-                    
-                    var address = venue.location.address;
-                    var city = venue.location.city;
-                    var state = venue.location.state;
-
-                    fullAddress += address;
-                    fullAddress += ', ';
-                    fullAddress += city;
-                    fullAddress += ', ';
-                    fullAddress += state;
-
-                    console.log(fullAddress);
-                    bobaLocation.push(fullAddress);
-                });
-            }
-            // results after being filtered
-            console.log(bobaResults);
-
-            console.log(bobaLocation);
-
-            // add names to bobaHTML
-            for (var b = 0; b < bobaResults.length; b++) {
-                bobaHTML += bobaResults[b];
-                bobaHTML += '</br>';
-                bobaHTML += bobaLocation[b];
-                bobaHTML += '</br>';
-            }
-
+            getUserInput();
+            searchBoba(foursquaredata);
             // show results in div
             $('#boba-results').html(bobaHTML);
         }
     });
+
+    function getUserInput() {
+        $('#search').click(function() {
+            var userInput = $('#location').val();
+            location = userInput;
+            console.log(location);
+        });
+    }
+
+    function searchBoba(data) {
+        // show all venues under category id's 'bubble tea shop' and 'tea rooms'
+        console.log(data.response.venues);
+        // check first filter item
+        for (var i = 0; i < filters.length; i++) {
+            var filterItem = filters[i];
+            // go through all venues
+            data.response.venues.forEach(venue => {
+                // create variable to store shop name 
+                var shop = venue.name;
+
+                // go into categories list
+                for (var j = 0; j < venue.categories.length; j++) {
+                    // get each name
+                    var name = venue.categories[j].name;
+                    // if name matches filter item then add it to boba results array
+                    if (name == filterItem) {
+                        bobaResults.push(shop);
+                    }
+                }
+
+                // create variable to store full address and reset each time looping through each locatino so that additional locations do not add onto html variable before pushing into array
+                var fullAddress = '';
+
+                // get location parts of venue
+                
+                var address = venue.location.address;
+                var city = venue.location.city;
+                var state = venue.location.state;
+
+                fullAddress += address;
+                fullAddress += ', ';
+                fullAddress += city;
+                fullAddress += ', ';
+                fullAddress += state;
+
+                console.log(fullAddress);
+                bobaLocation.push(fullAddress);
+            });
+        }
+        // results after being filtered
+        console.log(bobaResults);
+        //results of boba locations, should be aligned with results in bobaResults
+        console.log(bobaLocation);
+
+        // add names to bobaHTML
+        for (var b = 0; b < bobaResults.length; b++) {
+            bobaHTML += bobaResults[b];
+            bobaHTML += '</br>';
+            bobaHTML += bobaLocation[b];
+            bobaHTML += '</br>';
+        }
+    }
+
 
     $(window).scroll(function() {
         var scrollTop = $(window).scrollTop();
