c02925f4507164b041990247e94895d164dd18b6
Linh Ly <lly23@live.unc.edu>
2018-12-03 11:06:44 -0500

    foursquare API working

--- js/script.js
+++ js/script.js
@@ -1,36 +1,34 @@
 $(function() {
-    // FourSquare API
-    // fetch('https://api.foursquare.com/v2/venues/explore?client_id=' + config.CLIENT_ID + '&client_secret=' + config.CLIENT_SECRET + '&v=20180323&limit=1&ll=40.7243,-74.0018&query=coffee')
-    // .then(function() {
-    //     // Code for handling API response
-    //     var searchurl = 'https://api.foursquare.com/v2/venues/search';
-    //     $.ajax({
-    //         url: url,
-    //         data: foursquaredata,
-    //         type: 'GET',
-    //         dataType: 'json',
-    //         async: true,
-    //         success: function(foursquaredata) {
-    //             console.log(foursquaredata);
-    //         }
-    //     });    
-    // })
-    // .catch(function() {
-    //     // Code for handling errors
-    // });
-
-    var location = 'Chapel Hill, NC';
-    var searchURL = 'https://api.foursquare.com/v2/venues/search?client_id=' + config.CLIENT_ID + '&client_secret=' + config.CLIENT_SECRET + '&v=20180323&limit=1&ll=40.7243,-74.0018&query=boba&near=' + location;
-    var foursquaredata = [];
-    
+    var location = 'Raleigh, NC';
+    // Geocode
+    var locationData = '';
     $.ajax({
-        url: searchURL,
+       url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=' + config.GOOGLE_API_KEY,
+       data: locationData,
+       type: 'GET',
+       dataType: 'json',
+       async: true, 
+       success: function(locationData) { 
+            console.log(locationData);
+       }
+    });
+    // FourSquare API
+    var foursquaredata = [];
+    var filters = ['Tea Room', 'Bubble Tea Shop'];
+
+    $.ajax({
+        url: 'https://api.foursquare.com/v2/venues/search?client_id=' + config.CLIENT_ID + '&client_secret=' + config.CLIENT_SECRET + '&v=20180323&query=tea&near=' + location,
         data: foursquaredata,
         type: 'GET',
         dataType: 'json',
         async: true,
         success: function(foursquaredata) {
-            console.log(foursquaredata);
+            console.log(foursquaredata.response);
+            for (var i = 0; i < filters.length; i++) {
+                foursquaredata.forEach(element => {
+                    
+                });
+            }
         }
     });
 
