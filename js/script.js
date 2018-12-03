6f36289c26b428eb74d11e311d35853f8ca6e19b
Linh Ly <lly23@live.unc.edu>
2018-12-03 10:10:54 -0500

    set up foursquare

--- js/script.js
+++ js/script.js
@@ -1,12 +1,42 @@
 $(function() {
     // FourSquare API
-    fetch('https://api.foursquare.com/v2/venues/explore?client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=20180323&limit=1&ll=40.7243,-74.0018&query=coffee')
-    .then(function() {
-        // Code for handling API response
-    })
-    .catch(function() {
-        // Code for handling errors
+    // fetch('https://api.foursquare.com/v2/venues/explore?client_id=' + config.CLIENT_ID + '&client_secret=' + config.CLIENT_SECRET + '&v=20180323&limit=1&ll=40.7243,-74.0018&query=coffee')
+    // .then(function() {
+    //     // Code for handling API response
+    //     var searchurl = 'https://api.foursquare.com/v2/venues/search';
+    //     $.ajax({
+    //         url: url,
+    //         data: foursquaredata,
+    //         type: 'GET',
+    //         dataType: 'json',
+    //         async: true,
+    //         success: function(foursquaredata) {
+    //             console.log(foursquaredata);
+    //         }
+    //     });    
+    // })
+    // .catch(function() {
+    //     // Code for handling errors
+    // });
+
+    var location = 'Chapel Hill, NC';
+    var searchURL = 'https://api.foursquare.com/v2/venues/search?client_id=' + config.CLIENT_ID + '&client_secret=' + config.CLIENT_SECRET + '&v=20180323&limit=1&ll=40.7243,-74.0018&query=boba&near=' + location;
+    var foursquaredata = [];
+    
+    $.ajax({
+        url: searchURL,
+        data: foursquaredata,
+        type: 'GET',
+        dataType: 'json',
+        async: true,
+        success: function(foursquaredata) {
+            console.log(foursquaredata);
+        }
     });
+
+    function searchBoba(location, foursquaredata) {
+
+    }
 
     $(window).scroll(function() {
         var scrollTop = $(window).scrollTop();
