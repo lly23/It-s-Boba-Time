01c84a987b2731924579f50f04d3bad77f328bf4
Linh Ly <lly23@live.unc.edu>
2018-12-03 19:27:14 -0500

    radius and city, state works

--- js/script.js
+++ js/script.js
@@ -1,50 +1,64 @@
 $(function() {
     // location is global variable, will change based on user input
-    var location = 'Chapel Hill, NC';
+    var location = $('#location').val();
+    var radius = $('#radius').val();
+
+    $('#search').click(function() {
+        location = $('#location').val();
+        radius = $('#radius').val();
+        // calling foursquare AJAX call again for new location
+        $.ajax({
+            url: 'https://api.foursquare.com/v2/venues/search?client_id=' + config.FOURSQUARE_CLIENT_ID + '&client_secret=' + config.FOURSQUARE_CLIENT_SECRET + '&v=20180323&categoryId=52e81612bcbc57f1066b7a0c,4bf58dd8d48988d1dc931735&radius=' + radius + '&near=' + location,
+            data: foursquaredata,
+            type: 'GET',
+            dataType: 'json',
+            async: false,
+            success: function(foursquaredata) {
+                var results = searchBoba(foursquaredata);
+                // show results in div
+                $('#boba-results').html(results);
+            }
+        });
+    });
+
+    console.log(location);
 
     // Geocode
-    var locationData = '';
-    $.ajax({
-       url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=' + config.GOOGLE_API_KEY,
-       data: locationData,
-       type: 'GET',
-       dataType: 'json',
-       async: true, 
-       success: function(locationData) { 
-            console.log(locationData);
-       }
-    });
+    // var locationData = '';
+    // $.ajax({
+    //    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=' + config.GOOGLE_API_KEY,
+    //    data: locationData,
+    //    type: 'GET',
+    //    dataType: 'json',
+    //    async: true, 
+    //    success: function(locationData) { 
+    //         console.log(locationData);
+    //    }
+    // });
+
     // FourSquare API
     var foursquaredata = [];
-    // use these filters to narrow down search results
-    var filters = ['Bubble Tea Shop']; 
-    var bobaResults = [];
-    var bobaLocation = [];
-    var bobaHTML = '';
 
     $.ajax({
-        url: 'https://api.foursquare.com/v2/venues/search?client_id=' + config.CLIENT_ID + '&client_secret=' + config.CLIENT_SECRET + '&v=20180323&categoryId=52e81612bcbc57f1066b7a0c,4bf58dd8d48988d1dc931735&near=' + location,
+        url: 'https://api.foursquare.com/v2/venues/search?client_id=' + config.FOURSQUARE_CLIENT_ID + '&client_secret=' + config.FOURSQUARE_CLIENT_SECRET + '&v=20180323&categoryId=52e81612bcbc57f1066b7a0c,4bf58dd8d48988d1dc931735&radius=' + radius + '&near=' + location,
         data: foursquaredata,
         type: 'GET',
         dataType: 'json',
-        async: true,
+        async: false,
         success: function(foursquaredata) {
-            getUserInput();
-            searchBoba(foursquaredata);
+            var results = searchBoba(foursquaredata);
             // show results in div
-            $('#boba-results').html(bobaHTML);
+            $('#boba-results').html(results);
         }
     });
 
-    function getUserInput() {
-        $('#search').click(function() {
-            var userInput = $('#location').val();
-            location = userInput;
-            console.log(location);
-        });
-    }
-
     function searchBoba(data) {
+        var bobaResults = [];
+        var bobaLocation = [];
+        // use these filters to narrow down search results
+        var filters = ['Bubble Tea Shop', 'Boba']; 
+        var bobaHTML = '';
+
         // show all venues under category id's 'bubble tea shop' and 'tea rooms'
         console.log(data.response.venues);
         // check first filter item
@@ -59,34 +73,42 @@ $(function() {
                 for (var j = 0; j < venue.categories.length; j++) {
                     // get each name
                     var name = venue.categories[j].name;
+                    
                     // if name matches filter item then add it to boba results array
                     if (name == filterItem) {
+                        console.log(venue);
+                        // create variable to store full address and reset each time looping through each locatino so that additional locations do not add onto html variable before pushing into array
+                        var fullAddress = '';
+
+                        // get location parts of venue
+                        if (venue.location.address) {
+                            var address = venue.location.address;
+                            var city = venue.location.city;
+                            var state = venue.location.state;
+
+                            // add to full address
+                            fullAddress += address;
+                            fullAddress += ', ';
+                            fullAddress += city;
+                            fullAddress += ', ';
+                            fullAddress += state;
+                        } else {
+                            var address = venue.location.formattedAddress;
+
+                            // add to full address
+                            fullAddress += address;
+                        }
+                        
+                        // store shop name in bobaResults array and shop location in bobaLocation array
                         bobaResults.push(shop);
+                        bobaLocation.push(fullAddress);
                     }
                 }
-
-                // create variable to store full address and reset each time looping through each locatino so that additional locations do not add onto html variable before pushing into array
-                var fullAddress = '';
-
-                // get location parts of venue
-                
-                var address = venue.location.address;
-                var city = venue.location.city;
-                var state = venue.location.state;
-
-                fullAddress += address;
-                fullAddress += ', ';
-                fullAddress += city;
-                fullAddress += ', ';
-                fullAddress += state;
-
                 console.log(fullAddress);
-                bobaLocation.push(fullAddress);
             });
         }
         // results after being filtered
         console.log(bobaResults);
-        //results of boba locations, should be aligned with results in bobaResults
         console.log(bobaLocation);
 
         // add names to bobaHTML
@@ -96,7 +118,15 @@ $(function() {
             bobaHTML += bobaLocation[b];
             bobaHTML += '</br>';
         }
+
+        return bobaHTML;
     }
+
+
+
+
+
+
 
 
     $(window).scroll(function() {
